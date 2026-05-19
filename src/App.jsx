ebb4fd703610f6f76a9f793261f956ec61183968
Hani Kakadiya <hanikakadiya55@gmail.com>
2026-05-19 17:01:27 +0530

    ......

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Header from './Components/Header'
-import HeroSection from './Components/HeroSection'
 import AboutSection from './Components/AboutSection'
-
+import HeroSection from './Components/HeroSection'
 export default function App() {
   return (
     <>
